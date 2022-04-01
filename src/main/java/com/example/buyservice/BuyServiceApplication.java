4f17dee3ef9b45fae625ed8a0183d5cfdb978a7f
Vijay Kambala <kambalavijay@gmail.com>
2022-04-01 14:26:33 +0530

    first commit

---
+++ src/main/java/com/example/buyservice/BuyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.buyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/buyservice/BuyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
